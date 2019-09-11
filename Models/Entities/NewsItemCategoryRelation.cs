88e55021471d052018758cfae948dbac02eceef2
Heklaingolfs <thorbjorghekla94@gmail.com>
2019-09-11 12:31:33 +0000

    ymsar skrar

---
+++ Models/Entities/NewsItemCategoryRelation.cs
@@ -0,0 +1,7 @@
+namespace Models.Entities
+{
+    public class NewsItemCategoryRelation
+    {
+        
+    }
+}
---
+++ Models/HyperMedia/HypermediaLink.cs
@@ -0,0 +1,7 @@
+namespace Models.DTO
+{
+    public class HypermediaLink
+    {
+        
+    }
+}
